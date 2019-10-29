e283bc814f08f6107fbbdd8a2a2b136278a75a0d
Gusriandi <randimaskil9@gmail.com>
2019-10-29 13:38:19 +0700

    Update gns3_install.sh

--- gns3_install.sh
+++ gns3_install.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+##
+## GNS3 Installer
+##
+## Author : Gusriandi / Randy Filan
+## Email  : randimaskil9@gmail.com / randi@cit.co.id
+##
+## Licensed under GPL V3
+## Please refer to https://www.gnu.org/licenses/gpl-3.0.en.html
+##
+## How to use this script
+## 1. run the script using privileged user or using sudo command
+## 2. don't forget to pass the user of the program in the command argument
+##
+## example
+## sudo bash gns3_install.sh
+##
 sudo dpkg --add-architecture i386
 sudo add-apt-repository ppa:gns3/ppa
 sudo apt update                                
